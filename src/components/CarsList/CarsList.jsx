f89877909a0115ed3f668ddad583f326719cf62b
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-08-04 11:27:18 +0300

    fixs card list

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -11,7 +11,7 @@ export const CarsList = ({ catalog }) => {
       <Suspense fallback={<Loader name="Grid" />}>
         <Await resolve={catalog}>
           {resolvedCars =>
-            resolvedCars.map(el => {
+            resolvedCars?.map(el => {
               return <CarsListItem key={el.id} {...el} />;
             })
           }
