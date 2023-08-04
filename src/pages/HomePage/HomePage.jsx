d17c4e4e2f9ffba59c808c6ef9bd577ca8e530b7
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-08-04 20:50:09 +0300

    creates responsive and styling

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,44 +1,15 @@
-import img from 'assets/images/home_m.jpeg';
+import s from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1
-        style={{
-          marginTop: '40px',
-          textAlign: 'center',
-          textShadow: '-4px 5px 5px rgba(0,0,0,0.3)',
-        }}
-      >
-        Welcome to Car Rental 🚘
-      </h1>
-      <p
-        style={{
-          marginTop: '30px',
-          marginBottom: '60px',
-          marginLeft: '10%',
-          width: '50%',
-        }}
-      >
+    <div className={s.page}>
+      <h1 className={s.title}>Welcome to Car Rental 🚘</h1>
+      <p className={s.subtitle}>
         On the website, you can choose a car based on a photo and description,
         if you click the button Learn more, you will see detailed information
         about the car. You can add a car you like to your favorites and view the
         added cars on the favorites tab.
       </p>
-      <div
-        style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          backgroundImage: `url('${img}')`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          width: '100%',
-          height: 600,
-          borderRadius: 12,
-          boxShadow: ' 0px 10px 23px 0px rgba(0,0,0,0.75)',
-        }}
-      ></div>
     </div>
   );
 };
